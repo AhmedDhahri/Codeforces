ecd3564e7a453b3e1e3a90fc8ca67a17095ff260
Ahmed Dhahri <ahmed.dhahri17@gmail.com>
2019-09-17 12:12:14 +0100

    Add files via upload

--- r585_div2_prob_c.cpp
+++ r585_div2_prob_c.cpp
@@ -1,82 +1,68 @@
+
 #include <iostream>
 #include <string>
-#include <iomanip>
-
 
 using namespace std;
-void seq_map(char* s1, char* s2, int n, int* r, int* rn);
-int swap_count(int* r, int n);
 
 int main(){
-    int n;
-    do{
-        cin>>n;
-    }while((n<1)||(n>20000));
-    char t[n];char s[n];
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n,i;
+    cin>>n;
+    string s,t;
     cin>>t;
     cin>>s;
-    string out[n];int swp = 0; int rn;
-    int smap[n];
-    seq_map(t,s,n,smap,&rn);
-    for(int i=0;i<n;i++)
-        for(int j=0;j<n;j++)
-            if((smap[i] != 0)&&(smap[j] != 0))
-                if((smap[i] == smap[j])&&(i != j)){
-                        out[swp] = to_string(i+1)+" "+to_string(j+1);
-                        swp++;
-                        smap[i] = 0;smap[j] = 0;
-                        rn-=2;
-                        break;
-                    }
-        if(rn%2 != 0){
-            cout<<"-1";
-            return 0;
-        }
-        else if((rn == 2))
-            for(int i=0;i<n;i++)
-                for(int j=0;j<n;j++)
-                    if((smap[i] != 0)&&(smap[j] != 0)&&(smap[i] != smap[j])){
-                        out[swp] = to_string(i+1)+" "+to_string(i+1);
-                        swp++;
-                        out[swp] = to_string(i+1)+" "+to_string(j+1);
-                        swp++;
-                        smap[i] = 0;smap[j] = 0;
+    bool swp;
 
-                        break;
-                    }
+    int ab[n];
+    int _ab = 0;
+    int ba[n];
+    int _ba = 0;
 
-    cout<<swp<<endl;
-    for(int i=0;i<swp;i++)
-        cout<<out[i]<<endl;
-    return 0;
-}
-
-
-int swap_count(int* r, int n){
-    int c = 0;
-    for(int i=0;i<n;i++)
-        if(r[i] == 0)
-            c++;
-    return (n-c);
-}
-
-void seq_map(char* s1, char* s2, int n, int* r, int* rn){
-    *rn =0;
-    for(int i=0;i<n;i++){
-        if(s1[i] == s2[i]){
-            r[i] = 0;
-            continue;
-        }
-
-        if((s1[i] == 'a')&& (s2[i] == 'b')){
-            r[i] = 1;
-            *rn+=1;
-            continue;
-        }
-        if((s1[i] == 'b')&& (s2[i] == 'a')){
-            r[i] = 2;
-            *rn+=1;
-            continue;
+    for(i=0;i<n;i++){
+        if((s[i] == 'a') && (t[i] == 'b'))
+            ab[_ab++] = i+1;
+        else if((s[i] == 'b') && (t[i] == 'a'))
+            ba[_ba++] = i+1;
+    }
+    if((_ab + _ba)%2 != 0){
+        cout<<"-1";
+        return 0;
+    }
+    if((_ab%2 == 0)&&(_ba%2 == 0)){cout<<(_ab+_ba)/2<<endl;}
+    else {cout<<((_ab+_ba)/2)+1<<endl;}
+    i = 0;
+    if(_ab%2 == 0) {
+        swp = false;
+        while(i<_ab){
+            cout<<ab[i]<<" "<<ab[i+1]<<endl;
+            i+=2;
         }
     }
+    else {
+        swp = true;
+        while(i<_ab-1){
+            cout<<ab[i]<<" "<<ab[i+1]<<endl;
+            i+=2;
+        }
+    }
+    i = 0;
+    if(_ba%2 == 0) {
+        while(i<_ba){
+            cout<<ba[i]<<" "<<ba[i+1]<<endl;
+            i+=2;
+        }
+    }
+    else {
+        while(i<_ba-1){
+            cout<<ba[i]<<" "<<ba[i+1]<<endl;
+            i+=2;
+        }
+    }
+    if(swp){
+        cout<<ba[_ba-1]<<" "<<ba[_ba-1]<<endl;
+        cout<<ab[_ab-1]<<" "<<ba[_ba-1]<<endl;
+    }
+    return 0;
 }
